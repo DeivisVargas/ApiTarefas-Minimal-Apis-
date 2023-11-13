5b8f278473162efdf857e364fd8400f41825fb64
Deivis_vargas <deivisvp@yahoo.com.br>
2023-11-13 16:22:07 -0300

    Adicionando classe de contexto e banco em memória

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//para usar o EF CORE em memória 
+builder.Services.AddDbContext<AppDbContext>(
+    opt => opt.UseInMemoryDatabase("TarefasDb") );
+
+
 
 //iniciando o projeto
 
@@ -16,30 +24,44 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/", () => "ola mundo");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("frases", async () => 
+    await new HttpClient().GetStreamAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+);
+
+
+//rota de tarefas 
+app.MapGet("/tarefas" ,  async(AppDbContext db) =>{
+    return await db.Tarefas.ToListAsync();
+});
+
+app.MapPost("/tarefas" , async(Tarefa tarefa , AppDbContext db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}" , tarefa);
+} );
+
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+class Tarefa
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+
+    public string? Nome { get; set; }
+
+    public bool IsConcluida { get; set; }
+
 }
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
+
